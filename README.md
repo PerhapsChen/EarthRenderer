4ea845736e3b6edec6c75c3ffd94abdb1df8eb59
PerhapsChen <451622433@qq.com>
2023-03-27 01:12:13 +0800

    ToT

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EarthRenderer
+
+曾经的爱好，简单记录一下
+
+使用`xmake`构建的`OpenGL`渲染地球 实现[法线贴图](https://www.solarsystemscope.com/textures/)
+
+![image-20230327011152406](https://raw.githubusercontent.com/PerhapsChen/picgo_pic/main/image-20230327011152406.png)
--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "SolorSystem", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Earth", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
